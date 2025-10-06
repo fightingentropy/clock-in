ec8131e93df22083dbb9bc1b22488045078c9f70
Erlin Hoxha <erlinhoxha@MBP16M4.local>
2025-10-06 21:28:24 +0100

    .

---
+++ types/pg.d.ts
@@ -0,0 +1,84 @@
+declare module "pg" {
+  import { EventEmitter } from "node:events";
+
+  export interface PoolConfig {
+    connectionString?: string;
+    ssl?:
+      | boolean
+      | {
+          ca?: string;
+          key?: string;
+          cert?: string;
+          rejectUnauthorized?: boolean;
+        };
+    [key: string]: unknown;
+  }
+
+  export interface QueryResultRow {
+    [column: string]: unknown;
+  }
+
+  export interface QueryField {
+    name: string;
+    tableID: number;
+    columnID: number;
+    dataTypeID: number;
+    dataTypeSize: number;
+    dataTypeModifier: number;
+    format: "text" | "binary";
+  }
+
+  export type QueryCommand =
+    | "SELECT"
+    | "INSERT"
+    | "UPDATE"
+    | "DELETE"
+    | "MERGE";
+
+  export interface QueryResult<R extends QueryResultRow = QueryResultRow> {
+    rows: R[];
+    rowCount: number;
+    command: QueryCommand;
+    oid: number;
+    fields: QueryField[];
+  }
+
+  export interface QueryConfig<R extends QueryResultRow = QueryResultRow> {
+    text: string;
+    values?: ReadonlyArray<unknown>;
+    rowMode?: "array";
+    types?: unknown;
+  }
+
+  export interface Cursor<R extends QueryResultRow = QueryResultRow> {
+    read(rows: number): Promise<R[]>;
+    close(): Promise<void>;
+  }
+
+  export interface PoolClient extends EventEmitter {
+    query<R extends QueryResultRow = QueryResultRow>(
+      sql: string,
+      parameters?: ReadonlyArray<unknown>,
+    ): Promise<QueryResult<R>>;
+    query<R extends QueryResultRow = QueryResultRow>(
+      config: QueryConfig<R>,
+    ): Promise<QueryResult<R>>;
+    query<R extends QueryResultRow = QueryResultRow>(
+      cursor: Cursor<R>,
+    ): Cursor<R>;
+    release(err?: Error): void;
+  }
+
+  export class Pool extends EventEmitter {
+    constructor(config?: PoolConfig);
+    connect(): Promise<PoolClient>;
+    query<R extends QueryResultRow = QueryResultRow>(
+      sql: string,
+      parameters?: ReadonlyArray<unknown>,
+    ): Promise<QueryResult<R>>;
+    query<R extends QueryResultRow = QueryResultRow>(
+      config: QueryConfig<R>,
+    ): Promise<QueryResult<R>>;
+    end(): Promise<void>;
+  }
+}
